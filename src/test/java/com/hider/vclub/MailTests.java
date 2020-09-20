4a043593f786bea6ef5095801916b1311d8193a0
Hider <724598894@qq.com>
2020-09-20 10:36:13 +0800

    完成了邮件发送功能

---
+++ src/test/java/com/hider/vclub/MailTests.java
@@ -0,0 +1,38 @@
+package com.hider.vclub;
+
+import com.hider.vclub.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = VclubApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("724598894@qq.com", "test", "Welcome");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "Hider");
+
+        String content = templateEngine.process("/mail/test", context);
+        System.out.println(content);
+        mailClient.sendMail("724598894@qq.com", "test", content);
+
+    }
+}
